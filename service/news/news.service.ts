5a647eab553a7c6df0ad9797716ee2a1752a39e8
NZ-WEB <stereo00@yandex.ru>
2022-03-11 11:07:04 +0300

    Added news service

---
+++ service/news/news.service.ts
@@ -0,0 +1,62 @@
+import { SERVER_BASE_URL } from '../../utils/constants';
+import axios from 'axios';
+import { HomePageNewsInterface } from '../../interfaces/HomePageNews.interface';
+import { NewsInterface } from '../../interfaces/News.interface';
+
+export default class NewsService {
+  async getAll() {
+    try {
+      const response = await axios.get(`${SERVER_BASE_URL}/allNews/news`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      return response.data.allNews;
+    } catch (error: any) {
+      throw error.response.data.message;
+    }
+  }
+
+  async create(data: NewsInterface) {
+    try {
+      const token = JSON.parse(localStorage.getItem('user')).token;
+
+      const body = JSON.stringify({
+        news: data,
+      });
+
+      const response = await axios.post(
+        `${SERVER_BASE_URL}/allNews/news`,
+        body,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${token}`,
+          },
+        },
+      );
+      return response.data.news;
+    } catch (error: any) {
+      throw error.response.data.message;
+    }
+  }
+
+  async deleteById(id: number) {
+    try {
+      const token = JSON.parse(localStorage.getItem('user')).token;
+
+      const response = await axios.delete(
+        `${SERVER_BASE_URL}/allNews/news${id}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${token}`,
+          },
+        },
+      );
+      return response.data;
+    } catch (error: any) {
+      throw error.response.data.message;
+    }
+  }
+}
